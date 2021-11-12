cea06adffc88747978aedb99c06008a8ac11fe1f
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2021-11-12 09:04:58 -0300

    feat: :sparkles: created header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ export default function SignIn() {
           <Input label="Password" name="password" type="password" />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button type="submit" mt="6" bg="pink.500" size="lg">
           Entrar
         </Button>
       </Flex>
