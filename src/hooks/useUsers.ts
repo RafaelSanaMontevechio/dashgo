ec750db81f4d65b83161296768efb177d416e6db
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2022-01-11 09:26:56 -0300

    feat: :sparkles: add pagination

---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,46 @@
+import { useQuery } from 'react-query';
+
+import { api } from '../services/api';
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+type GetUsersResponse = {
+  totalCount: number;
+  users: User[];
+};
+
+async function getUsers(currentPage: number): Promise<GetUsersResponse> {
+  const { data, headers } = await api.get('/users', {
+    params: { page: currentPage },
+  });
+
+  const totalCount = Number(headers['x-total-count']);
+
+  const users = data.users.map((user) => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: new Date(user.created_at).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
+
+  return { users, totalCount };
+}
+
+export function useUsers(currentPage: number) {
+  return useQuery(
+    ['users', currentPage],
+    () => getUsers(currentPage),
+    { staleTime: 1000 * 5 }, //5 seconds
+  );
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useState } from 'react';
 
 import Link from 'next/Link';
 
@@ -26,30 +26,12 @@ import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
 
+import { useUsers } from '../../hooks/useUsers';
+
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
-    'users',
-    async () => {
-      const response = await fetch('http://localhost:3000/api/users');
-      const data = await response.json();
+  const [currentPage, setCurrentPage] = useState(1);
 
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          created_at: new Date(user.created_at).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        };
-      });
-
-      return users;
-    },
-    { staleTime: 1000 * 5 }, //5 seconds
-  );
+  const { data, isLoading, isFetching, error } = useUsers(currentPage);
 
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
@@ -63,6 +45,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -100,7 +85,7 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => (
+                  {data.users.map((user) => (
                     <Tr key={user.id}>
                       <Td px={['4', '4', '6']}>
                         <Checkbox colorScheme="pink" />
@@ -131,7 +116,11 @@ export default function UserList() {
                   ))}
                 </Tbody>
               </Table>
-              <Pagination />
+              <Pagination
+                totalCountOfItems={data.totalCount}
+                currentPage={currentPage}
+                onPageChange={setCurrentPage}
+              />
             </>
           )}
         </Box>
